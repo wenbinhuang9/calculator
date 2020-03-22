53dac4a2a5dd35a9ef3ff53fe29fe48f8953d3c8
wenbin huang <ben@wenbins-MacBook-Pro.local>
2020-03-22 11:51:34 -0700

    add suffix expression

---
+++ suffixexpr_test.py
@@ -0,0 +1,53 @@
+import unittest
+import re
+from suffixexpression import SuffixExpression
+class MyTestCase(unittest.TestCase):
+    def test_suffix_expr(self):
+        s ="1213+5)*6-(2 + 3)"
+
+        reg = "^([\)|[0-9]+|\+|\-|\*|/|\(|\)])"
+        pattern = re.compile(reg)
+        g = pattern.match(s)
+
+        print(g.group(1))
+
+        self.assertEqual(g.group((1)) == "1213", True)
+
+        s = ")"
+        pattern = re.compile(reg)
+        g = pattern.match(s)
+        print(g.group(1))
+
+    def test_parse(self):
+        s ="(1213+5)*6-(2 + 3)"
+        suffixcal = SuffixExpression()
+        correc_ans = [1213, 5, '+', 6, '*', 2, 3, '+', '-']
+
+        self.assertEqual(all([suffixcal[i] == correc_ans[i]  for i in range(len(correc_ans))]), True)
+
+
+    def test_calculate(self):
+        s = "(1213+5)*6-(2 + 3)"
+        ans =(1213+5)*6-(2 + 3)
+        suffixcal = SuffixExpression()
+
+        self.assertEqual(ans == suffixcal.calculate(s) , True)
+
+        s = "1- 3-10-3 +2"
+        ans = 1 - 3 - 10 - 3 + 2
+        print(suffixcal.parse(s))
+        print(suffixcal.calculate(s))
+        print(ans)
+        self.assertEqual(ans == suffixcal.calculate(s), True)
+
+        s = "4/1*2"
+        ans = 4/1*2
+        self.assertEqual(ans == suffixcal.calculate(s), True)
+
+        s = "4/(1*2)"
+        ans = 4 / (1 * 2)
+        self.assertEqual(ans == suffixcal.calculate(s), True)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ suffixexpression.py
@@ -0,0 +1,106 @@
+
+import re
+class SuffixExpression:
+    def __init__(self):
+        self.precedence = {
+            "(":10,
+            "*":9,
+            "/":9,
+            "+":8,
+            "-":8,
+            ")":7
+        }
+
+    def __check_valid(self):
+        pass
+
+    def __cmp_precedence(self, op1, op2):
+        return self.precedence[op1] - self.precedence[op2]
+
+    def __put_to_operator(self, suffix, operators, operator):
+        if operator == "(":
+            operators.append(operator)
+        elif operator == ")":
+            op_in_stack = operators.pop()
+            while len(operators) > 0 and op_in_stack != "(":
+                suffix.append(op_in_stack)
+                op_in_stack = operators.pop()
+        else:
+            if len(operators) > 0 and operators[-1] == "(":
+                operators.append(operator)
+                return
+
+            while len(operators) > 0 and self.__cmp_precedence(operator,operators[-1]) <= 0:
+                op_in_top = operators.pop()
+                suffix.append(op_in_top)
+
+            operators.append(operator)
+
+
+    def parse(self, expr):
+        # (3+5)*6 - (2 + 3)
+        reg = "^([0-9]+|\+|\-|\*|/|\(|\))"
+        pattern = re.compile(reg)
+
+        suffix = []
+        operators = []
+        try:
+            while expr != "":
+                match_ans = pattern.match(expr)
+                match_str = match_ans.group(1)
+
+                if match_str in self.precedence:
+                    self.__put_to_operator(suffix, operators, match_str)
+                else:
+                    # it should be an integer here
+                    number = int(match_str)
+                    suffix.append(number)
+
+                expr = expr[len(match_str):]
+                expr = expr.strip()
+
+            while len(operators) > 0:
+                suffix.append(operators.pop())
+            return suffix
+        except Exception as e:
+            print (e)
+            raise ValueError("invalid expression {0}".format(expr))
+
+    def __simulate(self, operand1, operand2, op):
+        if op not in self.precedence:
+            raise ValueError("invalid operator={0}", op)
+
+        if op == "+":
+            return  operand1  + operand2
+        elif op == "-":
+            return  operand1 - operand2
+        elif op == "*":
+            return  operand1 * operand2
+        elif op == "/":
+            return operand1/operand2
+
+        else:
+            raise ValueError("invalude operator={0}", op)
+
+
+
+    def calculate(self, expr):
+        suffix = self.parse(expr)
+
+        stack = []
+        ans = None
+        try:
+            for i in suffix:
+                if i in self.precedence:
+                    operand2 = stack.pop()
+                    operand1 = stack.pop()
+
+                    ans = self.__simulate(operand1, operand2, i)
+                    stack.append(ans)
+                else:
+                    stack.append(i)
+
+            return ans
+        except Exception as e:
+            print(e)
+            raise  ValueError("invalid suffix {0}", suffix)
